858d7aceeda1ace0ac216b3a3d0f2066bf965f71
arekk <arkadiuszkicior@gmail.com>
2017-02-27 21:46:18 +0100

    new titlle

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// tutaj znajduje się komentarz liniowy, nie można w nim używać enterów,
+// chyba, że zrobimy poniżej kolejny
+
+// alert('test');
+// powyższa funkcja została zakomentowana i nie zostanie wykonana
+
+var name = prompt('Witaj na mojej stronie - podaj swoje imię');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
